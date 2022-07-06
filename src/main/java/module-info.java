def0cf75ee7528fa7e3ca62746ba75748060d9e2
kien2001 <kienlevan2001@gmail.com>
2022-07-06 15:23:10 +0700

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.example.demo {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.demo to javafx.fxml;
+    exports com.example.demo;
+    exports com.example.demo.Controller;
+    opens com.example.demo.Controller to javafx.fxml;
+}
